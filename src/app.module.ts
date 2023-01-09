220de97d89f5e98f472894892eeca6f518b6a8a4
JessicaSandoval <jessica.sandoval@fsfb.org.co>
2023-01-08 20:47:24 -0500

    modular programming course platzi store

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,13 +1,22 @@
-import { Module } from '@nestjs/common';
+import { Module, HttpModule, HttpService } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { ProductsController } from './controllers/products.controller';
-import { CategoriesController } from './controllers/categories.controller';
-import { ProductsService } from './services/products.service';
-
+import { AppService } from './app.service';;
+import { UsersModule } from './users/users.module';
+import { ProductsModule } from './products/products.module';
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [],
-  controllers: [AppController, ProductsController, CategoriesController],
-  providers: [AppService, ProductsService],
+  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
+  controllers: [AppController],
+  providers: [
+    AppService,
+    {
+      provide: 'TASKS',
+      useFactory: async (http: HttpService) =>{
+        const tasks = await http.get('https://jsonplaceholder.typicode.com/todos').toPromise();
+        return tasks.data;
+      },
+      inject: [HttpService]
+    }
+  ],
 })
 export class AppModule {}
--- src/products/controllers/products.controller.ts
+++ src/products/controllers/products.controller.ts
@@ -14,14 +14,14 @@ import {
 } from '@nestjs/common';
 
 import { Response } from 'express';
-import { ParseIntPipe } from '../common/parse-int.pipe';
-import { CreateProductDto, UpdateProductDto } from './../dtos/products.dtos';
+import { ParseIntPipe } from '../../common/parse-int.pipe';
+import { CreateProductDto, UpdateProductDto } from '../dtos/products.dtos';
 
 import { ProductsService } from './../services/products.service';
 
 @Controller('products')
 export class ProductsController {
-  constructor(private productsService: ProductsService) {}
+  constructor(private productsService: ProductsService) { }
 
   @Get()
   getProducts(
