c933ffc053865e10bb70a358c3b00e1bbdab0f05
JessicaSandoval <jessica.sandoval@fsfb.org.co>
2023-01-08 21:08:33 -0500

    config_module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,18 @@
 import { Module, HttpModule, HttpService } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+
 import { AppController } from './app.controller';
 import { AppService } from './app.service';;
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [HttpModule, UsersModule, ProductsModule, DatabaseModule],
+  imports: [ 
+    ConfigModule.forRoot({
+      envFilePath: '.env',
+      isGlobal: true,
+    }),
+    HttpModule, UsersModule, ProductsModule, DatabaseModule ],
   controllers: [AppController],
   providers: [
     AppService,
--- src/users/services/users.service.ts
+++ src/users/services/users.service.ts
@@ -1,4 +1,5 @@
 import { Injectable, NotFoundException, Inject } from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
 
 import { User } from '../entities/user.entity';
 import { Order } from '../entities/order.entity';
@@ -9,7 +10,9 @@ import { ProductsService } from './../../products/services/products.service'
 @Injectable()
 export class UsersService {
  
-  constructor(private productsService: ProductsService, @Inject('API_KEY') private apiKey: string) {};
+  constructor(private productsService: ProductsService,
+    private configService: ConfigService,
+    ) {};
 
   private counterId = 1;
   private users: User[] = [
@@ -22,6 +25,9 @@ export class UsersService {
   ];
 
   findAll() {
+    const apiKey = this.configService.get('API_KEY');
+    const dbName = this.configService.get('DATABASE_NAME');
+    console.log(apiKey,dbName)
     return this.users;
   }
 
