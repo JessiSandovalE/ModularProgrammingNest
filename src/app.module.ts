504b278e5dfe46011c0b54d9729d4c7921b0e130
JessicaSandoval <jessica.sandoval@fsfb.org.co>
2023-01-10 17:57:05 -0500

    tipado en config

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,10 +6,14 @@ import { AppService } from './app.service';;
 import { UsersModule } from './users/users.module';
 import { ProductsModule } from './products/products.module';
 import { DatabaseModule } from './database/database.module';
+import config from './config';
+
+import { enviroments } from './enviroments'
 @Module({
   imports: [ 
     ConfigModule.forRoot({
-      envFilePath: '.env',
+      envFilePath: enviroments[process.env.NODE_ENV] || '.env',
+      load: [ config ],
       isGlobal: true,
     }),
     HttpModule, UsersModule, ProductsModule, DatabaseModule ],
